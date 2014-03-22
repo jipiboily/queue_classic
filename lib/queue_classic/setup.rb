b0c1e8e623362fb6d08edbb751926801febd762e
Jean-Philippe Boily <j@jipi.ca>
2014-03-22 11:25:46 -0400

    fix the connection sharing so it works as specified in README

--- lib/queue_classic/setup.rb
+++ lib/queue_classic/setup.rb
@@ -7,15 +7,15 @@ module QC
     AddHeartbeat = File.join(Root, "/sql/add_heartbeat.sql")
     RemoveHeartbeat = File.join(Root, "/sql/remove_heartbeat.sql")
 
-    def self.create(c=nil)
+    def self.create(c = QC::default_conn_adapter.connection)
       conn = QC::ConnAdapter.new(c)
       conn.execute(File.read(CreateTable))
       conn.execute(File.read(SqlFunctions))
-      Conn.execute(File.read(AddHeartbeat))
+      conn.execute(File.read(UpgradeTo_3_0_0))
       conn.disconnect if c.nil? #Don't close a conn we didn't create.
     end
 
-    def self.drop(c=nil)
+    def self.drop(c = QC::default_conn_adapter.connection)
       conn = QC::ConnAdapter.new(c)
       conn.execute("DROP TABLE IF EXISTS queue_classic_jobs CASCADE")
       conn.execute(File.read(DropSqlFunctions))
